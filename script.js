a1dd53955f112b280b3b211cf34efc6b6ca90bf6
Abhi <138367906+Parad0x-143@users.noreply.github.com>
2024-03-04 18:54:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ closeBtn.onclick = () =>{
 window.onscroll = () =>{
    navbar.classList.remove('active');
 };
+
+emailjs.init("JGGW7_aOaftGKXGz3"); // Replace with your EmailJS User ID
+
+const sendBtn = document.querySelector('.send-btn');
+const result = document.querySelector('.result');
+
+sendBtn.addEventListener('click', sendEmail);
+
+function sendEmail() {
+    // Get the form data
+    const to = document.getElementById("to").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    // Send the email using EmailJS
+    emailjs.send("service_oc9avt4", "template_b6fx66k", {
+        to_email: to,
+        subject: subject,
+        message: message
+    })
+        .then(function () {
+            result.innerHTML = "Email sent successfully!";
+            result.style.opacity = 1;
+        }, function (error) {
+            result.innerHTML = "Email sending failed!";
+            result.style.opacity = 1;
+        });
+}
